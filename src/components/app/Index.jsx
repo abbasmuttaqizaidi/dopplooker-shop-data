5b1d462889a7c1af03f2b3eaf486ce6a5ca7c93f
abbasmuttaqizaidi <zaidi.yam5@gmail.com>
2024-04-11 02:25:44 +0530

    header component added

--- src/components/app/Index.jsx
+++ src/components/app/Index.jsx
@@ -1,10 +1,11 @@
-import { useReducer, useState } from 'react';
+import { useReducer } from 'react';
 import { AppCustomerList } from '../app-customer-list/Index';
 import { AppDetails } from '../app-details/Index';
 import '../styles.css/styles.css';
 import { CustomerData } from '../../mockdata/mockdata';
 import { ShopManagementSystemContext } from '../context';
 import { appReducer } from '../context/reducer';
+import { Header } from '../header';
 
 
 export const ShopManagementSystem = () => {
@@ -12,14 +13,19 @@ export const ShopManagementSystem = () => {
     const [states, handleStates] = useReducer(appReducer, {
         customerData: CustomerData
     });
-    
+
     return (
         <ShopManagementSystemContext.Provider value={{
             states, handleStates
         }}>
             <div className='shopMS__container'>
-                <AppCustomerList />
-                <AppDetails />
+                <div className="shopMS__header">
+                    <Header />
+                </div>
+                <div className="shopMS__body">
+                    <AppCustomerList />
+                    <AppDetails />
+                </div>
             </div>
         </ShopManagementSystemContext.Provider>
     )
---
+++ src/components/header/index.jsx
@@ -0,0 +1,16 @@
+
+import { useState } from 'react';
+import './styles.css';
+export const Header = () => {
+
+    return (
+        <div className="header__container">
+            <button>Today's Purchase</button>
+            <button onClick={() => {
+
+            }}>Add Customer</button>
+        </div>
+    )
+}
+
+
