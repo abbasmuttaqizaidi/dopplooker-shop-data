356e7c8c9db9f8d2f02df597a60c888b78ef40a2
abbasmuttaqizaidi <zaidi.yam5@gmail.com>
2024-04-11 02:41:15 +0530

    modal code added

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,16 +1,39 @@
-
 import { useState } from 'react';
 import './styles.css';
 export const Header = () => {
-
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="header__container">
             <button>Today's Purchase</button>
             <button onClick={() => {
-
+                setIsOpen(!isOpen);
             }}>Add Customer</button>
+            <Modal
+                isOpen={isOpen}
+                onClose={() => {
+                    setIsOpen(!isOpen);
+                }}
+                title={"Add Customer"}
+            >
+                <p>hello</p>
+            </Modal>
         </div>
     )
 }
 
-
+export const Modal = ({ isOpen, onClose, children, title }) => {
+    return (
+        isOpen && <div className="modal__container">
+            <div className="modal__body">
+                <div className="modal__header">
+                    <div className="modal__title">
+                        <b>{title}</b>
+                    </div>
+                    <div className="modal__close" onClick={() => { onClose() }}>X</div>
+                </div>
+                <hr />
+                {children}
+            </div>
+        </div>
+    )
+}
