79039bce283494fa7d9a142166a6c5cf7bc60695
abbasmuttaqizaidi <zaidi.yam5@gmail.com>
2024-04-12 18:47:49 +0530

    todays sale design fixed

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,16 @@
 import './styles.css';
+import sales from '../../assets/icons/sales.svg';
+
 export const Header = () => {
 
     return (
         <div className="header__container">
             <div className="header__todayspurchase">
                 <div className="header__todayspurchase--container">
-                    <p className='header__todayspurchase--todayHeading'>Today's Sales</p>
+                    <p className='header__todayspurchase--todayHeading'>
+                        <span>Today's Sales</span>
+                        <img src={sales} className='img__small' alt='sales' />
+                    </p>
                     <div class="purchase__info">
                         <p className='purchase__info--heading'>Items Purchased</p>
                         <p className='purchase__info--value'>50</p>
