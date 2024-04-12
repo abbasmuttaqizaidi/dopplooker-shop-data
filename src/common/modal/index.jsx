29487263cac57b63d015f70305609f1a832c5182
abbasmuttaqizaidi <zaidi.yam5@gmail.com>
2024-04-12 20:28:53 +0530

    css added

--- src/common/modal/index.jsx
+++ src/common/modal/index.jsx
@@ -2,17 +2,17 @@ import './styles.css';
 
 export const Modal = ({ isOpen, onClose, children, title }) => {
     return (
-        isOpen && <div className="modal__container">
+        <div className={`modal__container ${isOpen ? 'show' : ''}`}>
             <div className="modal__body">
                 <div className="modal__header">
                     <div className="modal__title">
                         <b>{title}</b>
                     </div>
-                    <div className="modal__close" onClick={() => { onClose() }}>X</div>
+                    <div className="modal__close" onClick={onClose}>X</div>
                 </div>
                 <hr />
                 {children}
             </div>
         </div>
-    )
-}
+    );
+};
